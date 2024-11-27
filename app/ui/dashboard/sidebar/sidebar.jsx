2abe1390fce20a1b12153d5c92407c556466894d
MohammadAlmohsen <muhammadalmohsen8@gmail.com>
2024-11-28 00:36:45 +0200

    Prepare the basic files for the project and start with the sidebar

---
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styles from "./sidebar.module.css";
+
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+} from "react-icons/md";
+
+const menuItem = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/product",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Revenue",
+        path: "/dashboard/revenue",
+        icon: <MdWork />,
+      },
+      {
+        title: "Reports",
+        path: "/dashboard/reports",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Teams",
+        path: "/dashboard/teams",
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>
+      <ul>
+        {menuItem.map((cat)=>(
+            <li key={cat.title}><span className={styles.cat}>{cat.title}</span></li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
