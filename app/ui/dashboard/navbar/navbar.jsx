a087bc407c67daf73eb4077d8d5baed649f48c0c
MohammadAlmohsen <muhammadalmohsen8@gmail.com>
2024-11-30 23:12:22 +0200

    Start setting up your navigation bar

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-
+"use client";
+import React from "react";
+import styles from "./navbar.module.css";
+import { usePathname } from "next/navigation";
+import {
+  MdSearch,
+  MdPublic,
+  MdOutlineChat,
+  MdNotifications,
+} from "react-icons/md";
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
-}
+  const pathname = usePathname();
+
+  return (
+  <div className={styles.container}>
+    
+  </div>)
+};
 
 export default Navbar;
